c1743dc91f3eb69daffe0fc3a9bd80eb20b06f5d
JosueMateoHerrera <121052629+JosueMateoHerrera@users.noreply.github.com>
2023-01-04 12:33:27 -0500

    Add files via upload

---
+++ CODE/Login/src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login;
+
+/**
+ *
+ * @author josue
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        FormLogin objetoLogin = new FormLogin();
+        objetoLogin.setVisible(true);
+    }
+}
